7e49abf1d012aeab524a264d18c3e8ccebe26cbe
Joy <joyliao105@gmail.com>
2020-11-24 23:00:55 -0600

    debugging pipeline failure

--- index.js
+++ index.js
@@ -28,14 +28,13 @@
 //   })
 //   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const http = require("http");
+// const http = require("http");
 const express = require("express");
 const path = require("path");
 const app = express();
-app.use(express.json());
-app.use(express.static("express"));
+// app.use(express.json());
+// app.use(express.static("express"));
 // default URL for website
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
-const server = http.createServer(app);
