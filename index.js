c6ef317d27af30b7ac564b45aa0659246611d523
Joy <joyliao105@gmail.com>
2020-11-27 11:42:54 -0600

    debugging pipeline failure

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
-// const express = require("express");
-// const path = require("path");
-// const PORT = process.env.PORT || 5000;
-// const { Pool } = require("pg");
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
+var express = require("express");
+var app = express();
+var path = require("path");
 
-// express()
-//   .use(express.static(path.join(__dirname, "public")))
-//   .set("views", path.join(__dirname, "views"))
-//   .set("view engine", "ejs")
-//   .get("/", (req, res) => res.render("index"))
-//   .get("/db", async (req, res) => {
-//     try {
-//       const client = await pool.connect();
-//       const result = await client.query("SELECT * FROM test_table");
-//       const results = { results: result ? result.rows : null };
-//       res.render("pages/db", results);
-//       client.release();
-//     } catch (err) {
-//       console.error(err);
-//       res.send("Error " + err);
-//     }
-//   })
-//   .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-// const http = require("http");
-const express = require("express");
-const path = require("path");
-const app = express();
-// app.use(express.json());
-// app.use(express.static("express"));
-// default URL for website
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(process.env.PORT || 4000, function () {
+  console.log("Node app is working!");
 });
